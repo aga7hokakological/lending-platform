0738ff4dc0539b0b9c805e09f1313cee509162a4
aga7hokakological <iamsaurabhmandavkar@gmail.com>
2023-09-21 09:05:48 +0530

    staking contract updated

--- programs/staking-program/src/lib.rs
+++ programs/staking-program/src/lib.rs
@@ -8,199 +8,214 @@ declare_id!("EKqX4UHZW8LjUpyte77f6U1TydESn2dJQb6NGA91UBin");
 pub mod staking_program {
     use super::*;
  
-    pub fn initialize(ctx: Context<Initialize>, start_slot: u64, end_slot: u64) -> Result<()> {
-        msg!("Instruction: Initialize");
- 
-        let pool_info = &mut ctx.accounts.pool_info;
- 
-        pool_info.admin = ctx.accounts.admin.key();
-        pool_info.start_slot = start_slot;
-        pool_info.end_slot = end_slot;
-        pool_info.token = ctx.accounts.staking_token.key();
- 
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let escrow_account = &mut ctx.accounts.escrow_account;
+
+        escrow_account.admin = ctx.accounts.admin.key();
+        escrow_account.token = ctx.accounts.staking_token.key();
+        escrow_account.total_amount = 0;
+
         Ok(())
     }
- 
+
     pub fn stake(ctx: Context<Stake>, amount: u64) -> Result<()> {
-        msg!("Instruction: Stake");
- 
-        let user_info = &mut ctx.accounts.user_info;
-        let clock = Clock::get()?;
- 
-        if user_info.amount > 0 {
-            let reward = (clock.slot - user_info.deposit_slot) - user_info.reward_debt;
- 
+        let user_account = &mut ctx.accounts.user_account;
+
+        let clock_time = Clock::get()?;
+
+        if user_account.amount > 0 {
+            let reward = (clock_time.slot - user_account.deposit_time) - user_account.reward_debt;
+
+            // MAYBE CREATE A LOGIC BUG HERE
             let cpi_accounts = MintTo {
                 mint: ctx.accounts.staking_token.to_account_info(),
-                to: ctx.accounts.user_staking_wallet.to_account_info(),
+                to: ctx.accounts.user_staking_account.to_account_info(),
                 authority: ctx.accounts.admin.to_account_info(),
             };
-            let cpi_program = ctx.accounts.token_program.to_account_info();
+            let cpi_program = ctx.accounts.token_account.to_account_info();
             let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
             token::mint_to(cpi_ctx, reward)?;
         }
- 
+
         let cpi_accounts = Transfer {
-            from: ctx.accounts.user_staking_wallet.to_account_info(),
-            to: ctx.accounts.admin_staking_wallet.to_account_info(),
+            from: ctx.accounts.user_staking_account.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
             authority: ctx.accounts.user.to_account_info(),
         };
-        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_program = ctx.accounts.token_account.to_account_info();
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
         token::transfer(cpi_ctx, amount)?;
- 
-        user_info.amount += amount;
-        user_info.deposit_slot = clock.slot;
-        user_info.reward_debt = 0;
- 
+
+        user_account.amount += amount;
+        user_account.deposit_time = clock_time.slot;
+        user_account.reward_debt = 0;
+
         Ok(())
     }
- 
+
     pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
-        msg!("Instruction: Unstake");
- 
-        let user_info = &mut ctx.accounts.user_info;
-        let clock = Clock::get()?;
- 
-        let reward = (clock.slot - user_info.deposit_slot) - user_info.reward_debt;
- 
+        let user_account = &mut ctx.accounts.user_account;
+
+        let clock_time = Clock::get()?;
+
+        let reward = (clock_time.slot - user_account.deposit_time) - user_account.reward_debt;
+
         let cpi_accounts = MintTo {
             mint: ctx.accounts.staking_token.to_account_info(),
-            to: ctx.accounts.user_staking_wallet.to_account_info(),
+            to: ctx.accounts.user_staking_account.to_account_info(),
             authority: ctx.accounts.admin.to_account_info(),
         };
-        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_program = ctx.accounts.token_account.to_account_info();
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
         token::mint_to(cpi_ctx, reward)?;
- 
+
         let cpi_accounts = Transfer {
-            from: ctx.accounts.admin_staking_wallet.to_account_info(),
-            to: ctx.accounts.user_staking_wallet.to_account_info(),
+            from: ctx.accounts.token_account.to_account_info(),
+            to: ctx.accounts.user_staking_account.to_account_info(),
             authority: ctx.accounts.admin.to_account_info(),
         };
-        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_program = ctx.accounts.token_account.to_account_info();
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-        token::transfer(cpi_ctx, user_info.amount)?;
- 
-        user_info.amount = 0;
-        user_info.deposit_slot = 0;
-        user_info.reward_debt = 0;
- 
+        token::transfer(cpi_ctx, user_account.amount)?;
+
+        user_account.amount += 0;
+        user_account.deposit_time = 0;
+        user_account.reward_debt = 0;
+
         Ok(())
     }
- 
-    pub fn claim_reward(ctx: Context<ClaimReward>) -> Result<()> {
-        msg!("Instruction: Claim Reward");
- 
-        let user_info = &mut ctx.accounts.user_info;
-        let clock = Clock::get()?;
- 
-        let reward = (clock.slot - user_info.deposit_slot) - user_info.reward_debt;
- 
+
+    pub fn claim_rewards(ctx: Context<ClaimRewards>) -> Result<()> {
+        let user_account = &mut ctx.accounts.user_account;
+
+        let clock_time = Clock::get()?;
+
+        let reward = (clock_time.slot - user_account.deposit_time) - user_account.reward_debt;
+
         let cpi_accounts = MintTo {
             mint: ctx.accounts.staking_token.to_account_info(),
-            to: ctx.accounts.user_staking_wallet.to_account_info(),
+            to: ctx.accounts.user_staking_account.to_account_info(),
             authority: ctx.accounts.admin.to_account_info(),
         };
-        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_program = ctx.accounts.token_account.to_account_info();
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
         token::mint_to(cpi_ctx, reward)?;
- 
-        user_info.reward_debt += reward;
- 
+
+        user_account.reward_debt += reward;
+
         Ok(())
     }
 }
- 
+
 #[derive(Accounts)]
 pub struct Initialize<'info> {
+    /// The owner of the escrow account
     #[account(mut)]
     pub admin: Signer<'info>,
-    #[account(init, payer = admin, space = 8 + PoolInfo::LEN)]
-    pub pool_info: Account<'info, PoolInfo>,
+    /// Escrow account in which the tokens are to be staked
+    #[account(init, payer = admin, space = 8 + Escrow::LEN)]
+    pub escrow_account: Account<'info, Escrow>,
+    /// The token which is going to be staked
     #[account(mut)]
     pub staking_token: InterfaceAccount<'info, Mint>,
+    /// ATA of the token for admin
     #[account(mut)]
-    pub admin_staking_wallet: InterfaceAccount<'info, TokenAccount>,
-    pub system_program: Program<'info, System>,
+    pub token_account: InterfaceAccount<'info, TokenAccount>,
+    pub system_program: Program<'info, System>
 }
- 
+
 #[derive(Accounts)]
 pub struct Stake<'info> {
+    /// User who is going to stake
     #[account(mut)]
     pub user: Signer<'info>,
-    /// CHECK:
+    /// User account who is going to stake
+    #[account(init, payer = user, space = 8 + User::LEN)]
+    pub user_account: Account<'info, User>,
+    /// CHECK: Just to check the owner
     #[account(mut)]
     pub admin: AccountInfo<'info>,
-    #[account(init, payer = user, space = 8 + UserInfo::LEN)]
-    pub user_info: Account<'info, UserInfo>,
-    #[account(mut)]
-    pub user_staking_wallet: InterfaceAccount<'info, TokenAccount>,
-    #[account(mut)]
-    pub admin_staking_wallet: InterfaceAccount<'info, TokenAccount>,
+    /// The token which is going to be staked
     #[account(mut)]
     pub staking_token: InterfaceAccount<'info, Mint>,
+    /// ATA of the token for user
+    #[account(mut)]
+    pub user_staking_account: InterfaceAccount<'info, TokenAccount>,
+    /// ATA of the token for admin
+    #[account(mut)]
+    pub token_account: InterfaceAccount<'info, TokenAccount>,
+    /// The token program for the token staking token
     pub token_program: Interface<'info, TokenInterface>,
-    pub system_program: Program<'info, System>,
+    pub system_program: Program<'info, System>
 }
- 
+
 #[derive(Accounts)]
 pub struct Unstake<'info> {
-    /// CHECK:
+    /// User who is going to stake
     #[account(mut)]
-    pub user: AccountInfo<'info>,
-    /// CHECK:
+    pub user: Signer<'info>,
+    /// User account who is going to stake
+    #[account(mut)]
+    pub user_account: Account<'info, User>,
+    /// CHECK: Just to check the owner
     #[account(mut)]
     pub admin: AccountInfo<'info>,
-    #[account(mut)]
-    pub user_info: Account<'info, UserInfo>,
-    #[account(mut)]
-    pub user_staking_wallet: InterfaceAccount<'info, TokenAccount>,
-    #[account(mut)]
-    pub admin_staking_wallet: InterfaceAccount<'info, TokenAccount>,
+    /// The token which is going to be staked
     #[account(mut)]
     pub staking_token: InterfaceAccount<'info, Mint>,
-    pub token_program: Interface<'info, TokenInterface>,
+    /// ATA of the token for user
+    #[account(mut)]
+    pub user_staking_account: InterfaceAccount<'info, TokenAccount>,
+    /// ATA of the token for admin
+    #[account(mut)]
+    pub token_account: InterfaceAccount<'info, TokenAccount>,
+    /// The token program for the token staking token
+    pub token_program: Interface<'info, TokenInterface>
 }
- 
+
 #[derive(Accounts)]
-pub struct ClaimReward<'info> {
-    /// CHECK:
+pub struct ClaimRewards<'info> {
+    /// User who is going to stake
     #[account(mut)]
-    pub user: AccountInfo<'info>,
-    /// CHECK:
+    pub user: Signer<'info>,
+    /// User account who is going to stake
+    #[account(mut)]
+    pub user_account: Account<'info, User>,
+    /// CHECK: Just to check the owner
     #[account(mut)]
     pub admin: AccountInfo<'info>,
-    #[account(mut)]
-    pub user_info: Account<'info, UserInfo>,
-    #[account(mut)]
-    pub user_staking_wallet: InterfaceAccount<'info, TokenAccount>,
-    #[account(mut)]
-    pub admin_staking_wallet: InterfaceAccount<'info, TokenAccount>,
+    /// The token which is going to be staked
     #[account(mut)]
     pub staking_token: InterfaceAccount<'info, Mint>,
+    /// ATA of the token for user
+    #[account(mut)]
+    pub user_staking_account: InterfaceAccount<'info, TokenAccount>,
+    /// ATA of the token for admin
+    #[account(mut)]
+    pub token_account: InterfaceAccount<'info, TokenAccount>,
+    /// The token program for the token staking token
     pub token_program: Interface<'info, TokenInterface>,
-}
- 
+} 
+
 #[account]
-pub struct PoolInfo {
+pub struct Escrow {
     pub admin: Pubkey,
-    pub start_slot: u64,
-    pub end_slot: u64,
     pub token: Pubkey,
+    pub total_amount: u64
+    // pub bump: u8
 }
- 
+
 #[account]
-pub struct UserInfo {
+pub struct User {
     pub amount: u64,
     pub reward_debt: u64,
-    pub deposit_slot: u64,
+    pub deposit_time: u64
 }
- 
-impl UserInfo {
+
+impl User {
     pub const LEN: usize = 8 + 8 + 8;
 }
  
-impl PoolInfo {
-    pub const LEN: usize = 32 + 8 + 8 + 32;
+impl Escrow {
+    pub const LEN: usize = 32 + 32 + 8 + 2;
 }
--- tests/staking-program.ts
+++ tests/staking-program.ts
@@ -1,16 +1,316 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { join } from "path";
+import { readFileSync } from "fs";import * as anchor from "@coral-xyz/anchor";
+import { BN, Program } from "@coral-xyz/anchor";
+import {
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+} from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID, TOKEN_2022_PROGRAM_ID, createMint, createAccount, MINT_SIZE, mintTo, getMint, createAssociatedTokenAccount, mintToChecked, TokenOwnerOffCurveError } from "@solana/spl-token";
 import { StakingProgram } from "../target/types/staking_program";
+import { assert } from "chai";
+import TransactionFactory from "@coral-xyz/anchor/dist/cjs/program/namespace/transaction";
 
-describe("staking-program", () => {
+describe("staking-program", async () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.StakingProgram as Program<StakingProgram>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+  const WALLET_PATH = join(process.env["HOME"]!, ".config/solana/id.json");
+  const admin = Keypair.fromSecretKey(
+    Buffer.from(JSON.parse(readFileSync(WALLET_PATH, { encoding: "utf-8" })))
+  );
+
+  const pool = anchor.web3.Keypair.generate();
+  const user = anchor.web3.Keypair.generate();
+
+  let token;
+  let adminTokenAccount;
+  let userTokenAccount;
+
+  // beforeEach(async () => {
+    // airdrop to user
+    // const airdropSignature = await provider.connection.requestAirdrop(
+    //   user.publicKey,
+    //   10 * LAMPORTS_PER_SOL
+    // );
+    // const latestBlockHash = await provider.connection.getLatestBlockhash();
+
+    // await provider.connection.confirmTransaction({
+    //   blockhash: latestBlockHash.blockhash,
+    //     lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    //     signature: airdropSignature,
+    // });
+
+    // let tokenMintPubkey = await createMint(
+    //   provider.connection,
+    //   admin,
+    //   admin.publicKey,
+    //   null,
+    //   9
+    // );
+    
+    // let mintAccount = await getMint(provider.connection, tokenMintPubkey);
+    // // console.log(mintAccount);
+  
+    // let adminTokenAccountTx = await createAssociatedTokenAccount(
+    //   provider.connection,
+    //   admin,
+    //   tokenMintPubkey,
+    //   admin.publicKey
+    // );
+
+    // let userTokenAccountTx = await createAssociatedTokenAccount(
+    //   provider.connection,
+    //   user,
+    //   tokenMintPubkey,
+    //   admin.publicKey
+    // )
+  
+    // let txhash = await mintToChecked(
+    //   provider.connection,
+    //   admin,
+    //   tokenMintPubkey,
+    //   admin.publicKey,
+    //   admin,
+    //   100e9,
+    //   9
+    // )
+    // console.log(txhash)
+
+    it("Is initialized!", async () => {
+      const airdropSignature = await provider.connection.requestAirdrop(
+        user.publicKey,
+        10 * LAMPORTS_PER_SOL
+      );
+      const latestBlockHash = await provider.connection.getLatestBlockhash();
+  
+      await provider.connection.confirmTransaction({
+        blockhash: latestBlockHash.blockhash,
+          lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+          signature: airdropSignature,
+      });
+  
+      let tokenMintPubkey = await createMint(
+        provider.connection,
+        admin,
+        admin.publicKey,
+        null,
+        9
+      );
+      
+      let mintAccount = await getMint(provider.connection, tokenMintPubkey);
+      // console.log(mintAccount);
+    
+      // let adminTokenAccountTx = await createAssociatedTokenAccount(
+      //   provider.connection,
+      //   admin,
+      //   tokenMintPubkey,
+      //   admin.publicKey
+      // );
+  
+      // let userTokenAccountTx = await createAssociatedTokenAccount(
+      //   provider.connection,
+      //   user,
+      //   tokenMintPubkey,
+      //   user.publicKey
+      // )
+
+      userTokenAccount = await createAccount(
+        provider.connection,
+        user,
+        tokenMintPubkey,
+        user.publicKey    
+      )
+      // console.log(userTokenAccount)
+
+      adminTokenAccount = await createAccount(
+        provider.connection,
+        admin,
+        tokenMintPubkey,
+        admin.publicKey
+      )
+      // console.log(adminTokenAccount)
+    
+      let txhash = await mintToChecked(
+        provider.connection,
+        admin,
+        tokenMintPubkey,
+        admin.publicKey,
+        admin,
+        100e9,
+        9
+      )
+      console.log(txhash)
+      // Add your test here.
+      const tx = await program.methods.initialize().accounts({
+        admin: admin.publicKey,
+        escrowAccount: pool.publicKey,
+        stakingToken: tokenMintPubkey,
+        tokenAccount: adminTokenAccount,
+        systemProgram: anchor.web3.SystemProgram.programId
+      }).
+      signers([admin, pool]).
+      rpc();
+
+      console.log("Your transaction signature", tx);
+    });
+  // })
 });
+
+// describe("solana-staking-blog", () => {
+//   // Configure the client to use the local cluster.
+//   const provider = anchor.AnchorProvider.env();
+//   anchor.setProvider(provider);
+
+//   const program = anchor.workspace.StakingProgram as Program<StakingProgram>;
+
+//   const WALLET_PATH = join(process.env["HOME"]!, ".config/solana/id.json");
+//   const admin = Keypair.fromSecretKey(
+//     Buffer.from(JSON.parse(readFileSync(WALLET_PATH, { encoding: "utf-8" })))
+//   );
+//   const user = Keypair.generate();
+//   const poolInfo = Keypair.generate();
+//   const userInfo = Keypair.generate();
+
+//   let token;
+//   let adminTokenAccount: PublicKey;
+//   let userTokenAccount: PublicKey;
+
+//   before(async () => {
+//     const airdropSignature = await provider.connection.requestAirdrop(
+//       user.publicKey,
+//       10 * LAMPORTS_PER_SOL
+//     );
+//     const latestBlockHash = await provider.connection.getLatestBlockhash();
+
+//     await provider.connection.confirmTransaction({
+//       blockhash: latestBlockHash.blockhash,
+//         lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+//         signature: airdropSignature,
+//     }
+//       // "confirmed"
+//     );
+
+//     token = await createMint(
+//       provider.connection,
+//       admin,
+//       admin.publicKey,
+//       null,
+//       9,
+//       // TOKEN_2022_PROGRAM_ID
+//     );
+
+//     adminTokenAccount = await createAccount(
+//       provider.connection, 
+//       admin,
+//       admin.publicKey,
+//       admin.publicKey
+//       );
+//     userTokenAccount = await createAccount(
+//       provider.connection,
+//       user,
+//       user.publicKey,
+//       user.publicKey
+//       );
+
+//     await mintTo(
+//       provider.connection,
+//       admin,
+//       token,
+//       userTokenAccount, 
+//       admin.publicKey, 
+//       1e10);
+//   });
+
+//   it("Initialize", async () => {
+//     let _adminTokenAccount = await token.getAccountInfo(adminTokenAccount);
+//     assert.strictEqual(_adminTokenAccount.amount.toNumber(), 0);
+
+//     const tx = await program.methods
+//       .initialize(new BN(1), new BN(1e10))
+//       .accounts({
+//         admin: admin.publicKey,
+//         poolInfo: poolInfo.publicKey,
+//         stakingToken: token.publicKey,
+//         adminStakingWallet: adminTokenAccount,
+//         systemProgram: SystemProgram.programId,
+//       })
+//       .signers([admin, poolInfo])
+//       .rpc();
+//       console.log(tx)
+//     console.log("Your transaction signature", tx);
+//   });
+
+//   it("Stake", async () => {
+//     let _userTokenAccount = await token.getAccountInfo(userTokenAccount);
+//     assert.strictEqual(_userTokenAccount.amount.toNumber(), 1e10);
+
+//     const tx = await program.methods
+//       .stake(new BN(1e10))
+//       .accounts({
+//         user: user.publicKey,
+//         admin: admin.publicKey,
+//         userInfo: userInfo.publicKey,
+//         userStakingWallet: userTokenAccount,
+//         adminStakingWallet: adminTokenAccount,
+//         stakingToken: token.publicKey,
+//         tokenProgram: TOKEN_PROGRAM_ID,
+//         systemProgram: SystemProgram.programId,
+//       })
+//       .signers([user, userInfo])
+//       .rpc();
+//     console.log("Your transaction signature", tx);
+
+//     let _adminTokenAccount = await token.getAccountInfo(adminTokenAccount);
+//     assert.strictEqual(_adminTokenAccount.amount.toNumber(), 1e10);
+//   });
+
+//   it("Claim Reward", async () => {
+//     let _adminTokenAccount = await token.getAccountInfo(adminTokenAccount);
+//     assert.strictEqual(_adminTokenAccount.amount.toNumber(), 1e10);
+
+//     const tx = await program.methods
+//       .claimReward()
+//       .accounts({
+//         user: user.publicKey,
+//         admin: admin.publicKey,
+//         userInfo: userInfo.publicKey,
+//         userStakingWallet: userTokenAccount,
+//         adminStakingWallet: adminTokenAccount,
+//         stakingToken: token.publicKey,
+//         tokenProgram: TOKEN_PROGRAM_ID,
+//       })
+//       .rpc();
+//     console.log("Your transaction signature", tx);
+
+//     let _userTokenAccount = await token.getAccountInfo(userTokenAccount);
+//     assert.strictEqual(_userTokenAccount.amount.toNumber(), 1);
+//   });
+
+//   it("Unstake", async () => {
+//     let _adminTokenAccount = await token.getAccountInfo(adminTokenAccount);
+//     assert.strictEqual(_adminTokenAccount.amount.toNumber(), 1e10);
+
+//     const tx = await program.methods
+//       .unstake()
+//       .accounts({
+//         user: user.publicKey,
+//         admin: admin.publicKey,
+//         userInfo: userInfo.publicKey,
+//         userStakingWallet: userTokenAccount,
+//         adminStakingWallet: adminTokenAccount,
+//         stakingToken: token.publicKey,
+//         tokenProgram: TOKEN_PROGRAM_ID,
+//       })
+//       .rpc();
+//     // console.log(tx)
+//     console.log("Your transaction signature", tx);
+
+//     let _userTokenAccount = await token.getAccountInfo(userTokenAccount);
+//     assert.strictEqual(_userTokenAccount.amount.toNumber(), 1e10 + 2);
+//   });
+// });
